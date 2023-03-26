55134c12a4f3e562deb2b7b1a3f9b83c3795e0f6
Chris <65196986+cjwolfe@users.noreply.github.com>
2023-03-26 09:24:37 -0400

    2023 03 26

--- Section 18 - Object Literals/objectlit.js
+++ Section 18 - Object Literals/objectlit.js
@@ -10,3 +10,36 @@ const restaurant = {
 //YOUR CODE GOES DOWN HERE:
 
 let fullAddress = `${restaurant.address}, ${restaurant.city}, ${restaurant.state} ${restaurant.zipcode}`;
+
+
+const comments = [
+    {
+        username: "Timmy",
+        text: "lolololol",
+        votes: 9,
+    },
+    {
+        username: "Johnny",
+        text: "lol lol lol lol",
+        votes: 5,
+    },
+    {
+        username: "Spike",
+        text: "lol",
+        votes: 3,
+    },
+    {
+        username: "Vorthos",
+        text: "In the 8th year of Draconis...",
+        votes: 2,
+    }
+]
+
+
+
+
+
+
+
+
+
