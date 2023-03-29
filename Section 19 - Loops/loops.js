19c4ac3bc2bbf6410dd03af6fc4bebde3bb08815
Chris <65196986+cjwolfe@users.noreply.github.com>
2023-03-29 19:26:14 -0400

    2023 03 29

--- Section 19 - Loops/loops.js
+++ Section 19 - Loops/loops.js
@@ -1,6 +1,14 @@
 // loops!
 //Basic bitch for loop
-for (let i = 1; i <= 69; i++) {
-    console.log(i);
+// for (let i = 1; i <= 69; i++) {
+//     console.log(i);
+// }
+
+for (let i = 0; i < 6; i++) {
+    console.log("Da ba dee da ba daa");
 }
 
+for (let i = 0; i < 21; i += 2) {
+    console.log(i)
+}
+for (let i = 25; i >= 0; i -= 5) { console.log(i) }
