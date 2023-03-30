e5e43c2181b4d9cff7b4d6f9df92a4ac7a4adf05
Chris <65196986+cjwolfe@users.noreply.github.com>
2023-03-30 10:05:57 -0400

    2023 03 30

--- Section 19 - Loops/loops.js
+++ Section 19 - Loops/loops.js
@@ -4,11 +4,28 @@
 //     console.log(i);
 // }
 
-for (let i = 0; i < 6; i++) {
-    console.log("Da ba dee da ba daa");
-}
+// for (let i = 0; i < 6; i++) {
+//     console.log("Da ba dee da ba daa");
+// }
 
-for (let i = 0; i < 21; i += 2) {
-    console.log(i)
-}
-for (let i = 25; i >= 0; i -= 5) { console.log(i) }
+// for (let i = 0; i < 21; i += 2) {
+//     console.log(i)
+// }
+// for (let i = 25; i >= 0; i -= 5) { console.log(i) }
+
+
+// looping over arrays
+// const newArray = ["Eenie", "Meenie", "Miney", "Moe"]
+// for (let index = 0; index < newArray.length; index++) {
+//     console.log(newArray[index]);
+
+// }
+// for (let index = newArray.length - 1; index >= 0; index--) {
+//     console.log(newArray[index]);
+// }
+// const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
+
+// // WRITE YOUR LOOP BELOW THIS LINE:
+// for (var i = 0; i < people.length; i++) {
+//     console.log(people[i].toUpperCase())
+// }
