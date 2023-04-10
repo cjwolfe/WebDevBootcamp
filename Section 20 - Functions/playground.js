8669cff36cf2503f127f6b3a5ddcc42e17702f33
Chris <65196986+cjwolfe@users.noreply.github.com>
2023-04-10 10:58:07 -0600

    2023 04 10
    Updated lots of folders. Functions!

---
+++ Section 20 - Functions/playground.js
@@ -0,0 +1,12 @@
+// Snake Eyes Activity
+
+function isSnakeEyes(firstDie, secondDie) {
+    if (firstDie === 1 && secondDie === 1) {
+        console.log("Snake Eyes!")
+    } else { console.log("Not Snake Eyes!") }
+
+}
+// Multiplication activity
+function multiply(x, y) {
+    return x * y;
+}
