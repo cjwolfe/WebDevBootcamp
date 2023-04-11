2e505f51e4b675aa2f3fc9f51ef6a08e8383517a
Chris <65196986+cjwolfe@users.noreply.github.com>
2023-04-11 16:00:26 -0600

    2023 04 11

--- Section 20 - Functions/playground.js
+++ Section 20 - Functions/playground.js
@@ -10,3 +10,42 @@ function isSnakeEyes(firstDie, secondDie) {
 function multiply(x, y) {
     return x * y;
 }
+
+//Capitalizes the first letter - with new string
+
+function capitalize(word) {
+    let firstHalf = word.slice(0, 1).toUpperCase();
+    let lastHalf = word.slice(1);
+    let result = firstHalf + lastHalf;
+    console.log(result)
+    return result;
+
+}
+
+//Here is a faster way to do the above, I mostly did my way because I was having trouble with toUpperCase
+function capitalizeVersion2(word) {
+    return word[0].toUpperCase() + word.slice(1);
+}
+
+//Returns the sum of an array
+
+function sumArray(numbers) {
+    let total = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        total += numbers[i];
+        console.log(total);
+    }
+    return total;
+
+}
+
+//return Day
+//Days in 1-7, reject if null
+
+function returnDay(day) {
+    if (day > 7 || day < 1) {
+        return null;
+    }
+    const bigDays = [null, "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    return bigDays[day];
+}
