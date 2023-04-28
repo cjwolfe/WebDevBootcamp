d4ea00561603753de0faf78ba6093a6f97878cf9
Chris <65196986+cjwolfe@users.noreply.github.com>
2023-04-27 21:10:49 -0400

    2023 04 27

--- Section 22 - Callbacks and Array Methods/playground.js
+++ Section 22 - Callbacks and Array Methods/playground.js
@@ -1,2 +1,11 @@
 const cats = ["Mittens", "Jareth", "Dorian"];
 
+// DO NOT ALTER THE FOLLOWING CODE:
+const fullNames = [{ first: 'Albus', last: 'Dumbledore' }, { first: 'Harry', last: 'Potter' }, { first: 'Hermione', last: 'Granger' }, { first: 'Ron', last: 'Weasley' }, { first: 'Rubeus', last: 'Hagrid' }, { first: 'Minerva', last: 'McGonagall' }, { first: 'Severus', last: 'Snape' }];
+
+// Write your code here
+
+const firstNames = fullNames.map(function (name) {
+    return name.first;
+})
+// returns first names only from fullNames array
