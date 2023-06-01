6f42ed102e26d673b83568444a72e5a75313ad21
Chris <65196986+cjwolfe@users.noreply.github.com>
2023-05-31 20:08:21 -0400

    2023 05 31

--- Section 24 - The DOM/playground.js
+++ Section 24 - The DOM/playground.js
@@ -1,3 +1,13 @@
 document.querySelector("#container").style.textAlign = "center";
 document.querySelector("img").style.width = "150px";
 document.querySelector("img").style.borderRadius = "50%";
+
+
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']; //PLEASE DON'T CHANGE THIS LINE!
+
+//YOU CODE GOES HERE:
+
+const settings = document.querySelectorAll("span");
+for (let i = 0; i < settings.length; i++) {
+    settings[i].style.color = colors[i];
+};
